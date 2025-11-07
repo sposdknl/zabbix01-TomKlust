4bf902a0b8b730395b745cde8c897d8925e8396a
TomKlust <tomklust@seznam.cz>
2025-11-07 15:50:03 +0100

    Update configure-zabbix-agent2.sh

--- configure-zabbix-agent2.sh
+++ configure-zabbix-agent2.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
-UNIQUE_HOSTNAME="ubuntu-$(uuidgen)"
-SHORT_HOSTNAME=$(echo $UNIQUE_HOSTNAME | cut -d'-' -f1,2)
+ZABBIX_SERVER="enceladus.pfsense.cz"
+HOSTNAME="spos-$(Klustt)-$(date +%s)"  
 
-# # Konfigurace zabbix_agent2.conf
-sudo cp -v /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf-orig
-sudo sed -i "s/Hostname=Zabbix server/Hostname=$Klustt/g" /etc/zabbix/zabbix_agent2.conf
-sudo sed -i 's/Server=127.0.0.1/Server=enceladus.pfsense.cz/g' /etc/zabbix/zabbix_agent2.conf
-sudo sed -i 's/ServerActive=127.0.0.1/ServerActive=enceladus.pfsense.cz/g' /etc/zabbix/zabbix_agent2.conf
-sudo sed -i 's/# Timeout=3/Timeout=30/g' /etc/zabbix/zabbix_agent2.conf
-sudo sed -i 's/# HostMetadata=/HostMetadata=SPOS/g' /etc/zabbix/zabbix_agent2.conf
-sudo diff -u /etc/zabbix/zabbix_agent2.conf-orig /etc/zabbix/zabbix_agent2.conf
+echo "=== Konfigurace Zabbix Agent2 ==="
 
-# Restart sluzby zabbix-agent2
+sudo bash -c "cat > /etc/zabbix/zabbix_agent2.conf" <<EOF
+Server=${ZABBIX_SERVER}
+ServerActive=${ZABBIX_SERVER}
+Hostname=${Klustt}
+HostMetadata=SPOS
+Include=/etc/zabbix/zabbix_agent2.d/*.conf
+EOF
+
 sudo systemctl restart zabbix-agent2
-
-# EOF
+sudo systemctl status zabbix-agent2 --no-pager
